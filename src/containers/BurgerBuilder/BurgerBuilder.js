560ae1d58a9bacb0b766c86650a64eef37524613
HamzaAlwan <ihamzaalwan@gmail.com>
2022-09-04 02:36:55 +0300

    Add authentication

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -33,7 +33,12 @@ class BurgerBuilder extends Component {
 	}
 
 	purchaseHandler = () => {
-		this.setState({ purchasing: true });
+		if (this.props.isAuthenticated) {
+			this.setState({ purchasing: true });
+		} else {
+			this.props.onSetAuthRedirectPath("/checkout");
+			this.props.history.push("/auth");
+		}
 	};
 
 	purchaseCancelHandler = () => {
@@ -71,6 +76,7 @@ class BurgerBuilder extends Component {
 						purchasable={this.updatePurchaseState(this.props.ings)}
 						ordered={this.purchaseHandler}
 						price={this.props.price}
+						isAuth={this.props.isAuthenticated}
 					/>
 				</Aux>
 			);
@@ -98,22 +104,27 @@ class BurgerBuilder extends Component {
 	}
 }
 
-const mapStateToProps = state => {
-    return {
-        ings: state.burgerBuilder.ingredients,
-        price: state.burgerBuilder.totalPrice,
-        error: state.burgerBuilder.error
-    };
-}
+const mapStateToProps = (state) => {
+	return {
+		ings: state.burgerBuilder.ingredients,
+		price: state.burgerBuilder.totalPrice,
+		error: state.burgerBuilder.error,
+		isAuthenticated: state.auth.token !== null,
+	};
+};
 
-const mapDispatchToProps = dispatch => {
-    return {
-        onIngredientAdded: (ingredientName) => dispatch(actions.addIngredient(ingredientName)),
-        onIngredientRemoved: (ingredientName) => dispatch(actions.removeIngredient(ingredientName)),
-        onInitIngredients: () => dispatch(actions.initIngredients()),
-        onInitPurchase: () => dispatch(actions.purchaseInit())
-    }
-}
+const mapDispatchToProps = (dispatch) => {
+	return {
+		onIngredientAdded: (ingredientName) =>
+			dispatch(actions.addIngredient(ingredientName)),
+		onIngredientRemoved: (ingredientName) =>
+			dispatch(actions.removeIngredient(ingredientName)),
+		onInitIngredients: () => dispatch(actions.initIngredients()),
+		onInitPurchase: () => dispatch(actions.purchaseInit()),
+		onSetAuthRedirectPath: (path) =>
+			dispatch(actions.setAuthRedirectPath(path)),
+	};
+};
 
 export default connect(
 	mapStateToProps,
--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -9,16 +9,23 @@ import Spinner from "../../components/UI/Spinner/Spinner";
 
 class Orders extends Component {
 	componentDidMount() {
-		this.props.onFetchOrders();
+		this.props.onFetchOrders(this.props.token, this.props.userId);
 	}
 
 	render() {
 		let orders = <Spinner />;
 		if (!this.props.loading) {
 			if (this.props.orders.length === 0) {
-				orders = <div style={{ textTransform: 'capitalize', textAlign: 'center' }}>
-                        <h1>No orders have been made yet.</h1>;
-                    </div>
+				orders = (
+					<div
+						style={{
+							textTransform: "capitalize",
+							textAlign: "center",
+						}}
+					>
+						<h1>No orders have been made yet.</h1>
+					</div>
+				);
 			} else {
 				orders = this.props.orders.map((order) => (
 					<Order
@@ -37,12 +44,15 @@ const mapStateToProps = (state) => {
 	return {
 		orders: state.order.orders,
 		loading: state.order.loading,
+		token: state.auth.token,
+		userId: state.auth.userId,
 	};
 };
 
 const mapDispatchToProps = (dispatch) => {
 	return {
-		onFetchOrders: () => dispatch(actions.fetchOrders()),
+		onFetchOrders: (token, userId) =>
+			dispatch(actions.fetchOrders(token, userId)),
 	};
 };
 
